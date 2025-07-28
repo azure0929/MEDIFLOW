801a02b2df2cffd69031f3622746c533de73518c
강민기 <131340181+KangMinGi92@users.noreply.github.com>
2025-07-28 12:33:05 +0900

    feat : 4개테이블에 대한 mapper작성 및 단위테스트 완료
    추가적으로 memberDAO와 Service HospitalDAO와 Service까지완료

--- MavenProject/src/main/java/com/service/spring/dao/MemberDAO.java
+++ MavenProject/src/main/java/com/service/spring/dao/MemberDAO.java
@@ -1,5 +1,48 @@
 package com.service.spring.dao;
 
-public class MemberDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.service.spring.domain.Member;
+
+@Repository
+public class MemberDAO {
+	public static final String NS = "MemberMapper.";
+	
+	@Autowired
+	private SqlSession session;
+	
+	public MemberDAO() {
+	}
+	
+	public Member logIn(Member member)throws Exception{
+		return session.selectOne(NS+"logIn",member);
+	}
+	
+	public void insertMember(Member member)throws Exception {
+		session.insert(NS+"insertMember",member);
+	}
+	
+	public void updateMember(Member member)throws Exception {
+		session.update(NS+"updateMember",member);
+	}
+	
+	public void deleteMember(int mId)throws Exception {
+		session.delete(NS+"deleteMember",mId);
+	}
+	
+	public List<Member> searchAllMember()throws Exception {
+		return session.selectList(NS+"searchAllMember");
+	}
+	
+	public Member searchMemberByNum(int mId)throws Exception{
+		return session.selectOne(NS+"searchMemberByNum",mId);
+	}
+	
+	public List<Member> searchMember(Member member)throws Exception{
+		return session.selectList(NS+"searchMember",member);
+	}
 }
